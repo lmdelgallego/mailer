58d3f255c86b750f79e84da85daa2dff4cb2e237
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-10-20 07:40:52 -0500

    📩  List Mails

--- app/mail.py
+++ app/mail.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, request, jsonify
+from app.db import get_db
 
 bp = Blueprint("mail", __name__, url_prefix="/")
 
 
 @bp.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    db, cursor = get_db()
+
+    cursor.execute("SELECT * FROM email")
+    mails = cursor.fetchall()
+
+    return render_template("index.html", mails=mails)
