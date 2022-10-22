c6d92371fe245c7e146bab1075a3ffd531a0e9f1
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-10-22 16:31:52 -0500

    👷🏽‍♂️ Form validation

--- app/mail.py
+++ app/mail.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, flash, render_template, request, jsonify
 from app.db import get_db
 
 bp = Blueprint("mail", __name__, url_prefix="/")
@@ -18,17 +18,31 @@ def index():
 def create():
     if request.method == "POST":
         db, cursor = get_db()
+        errors = []
 
         email = request.form["email"]
         subject = request.form["subject"]
         content = request.form["content"]
 
-        cursor.execute(
-            "INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)",
-            (email, subject, content),
-        )
-        db.commit()
+        if not email:
+            errors.append("Email is required.")
+        if not subject:
+            errors.append("Subject is required.")
+        if not content:
+            errors.append("Content is required.")
 
-        return jsonify({"success": True})
+        if len(errors) == 0:
+            pass
+        else:
+            for error in errors:
+                flash(error)
+
+        # cursor.execute(
+        #     "INSERT INTO email (email, subject, content) VALUES (%s, %s, %s)",
+        #     (email, subject, content),
+        # )
+        # db.commit()
+
+        # return jsonify({"success": True})
 
     return render_template("mails/create.html")
